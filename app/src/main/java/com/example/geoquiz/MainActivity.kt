1d4fc91075814cef4a8fa3c02886b8e0931d5b90
thejiexa <thejiexxa@gmail.com>
2023-03-07 01:16:15 +1100

    lab2

--- app/src/main/java/com/example/geoquiz/MainActivity.kt
+++ app/src/main/java/com/example/geoquiz/MainActivity.kt
@@ -2,11 +2,13 @@ package com.example.geoquiz
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.ImageButton
 import android.widget.TextView
 import android.widget.Toast
 
+private const val TAG = "MainActivity"
 class MainActivity : AppCompatActivity() {
 
     private lateinit var trueButton:Button
@@ -14,8 +16,8 @@ class MainActivity : AppCompatActivity() {
     //    private lateinit var nextButton:Button
     //    private lateinit var prevButton: Button
     private lateinit var questionTextView: TextView
-    private lateinit var nextButton:ImageButton
-    private lateinit var prevButton:ImageButton
+    private lateinit var nextImageButton:ImageButton
+    private lateinit var prevImageButton:ImageButton
 
     private var currentIndex = 0
 
@@ -28,8 +30,26 @@ class MainActivity : AppCompatActivity() {
         Question(R.string.question_asia, true)
     )
 
-
-
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "onStart() called")
+    }
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "onResume() called")
+    }
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "onPause() called")
+    }
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "onStop() called")
+    }
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy() called")
+    }
     private fun updateQuestion() {
         val questionTextResId = questionBank[currentIndex].textResId
         questionTextView.setText(questionTextResId)
@@ -45,12 +65,13 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.d(TAG, "onCreate(Bundle?) called")
         setContentView(R.layout.activity_main)
 
         trueButton = findViewById(R.id.true_button)
         falseButton = findViewById(R.id.false_button)
-        nextButton = findViewById(R.id.next_button)
-        prevButton = findViewById(R.id.prev_button)
+        nextImageButton = findViewById(R.id.next_button)
+        prevImageButton = findViewById(R.id.prev_button)
         questionTextView = findViewById(R.id.question_text_view)
 
         trueButton.setOnClickListener {
@@ -62,26 +83,24 @@ class MainActivity : AppCompatActivity() {
         }
 
         questionTextView.setOnClickListener {
-            if (currentIndex != questionBank.size - 1)
+            if (currentIndex < questionBank.size - 1)
                 currentIndex = (currentIndex + 1) % questionBank.size
 
             updateQuestion()
         }
 
-        nextButton.setOnClickListener{
+        nextImageButton.setOnClickListener{
 //            currentIndex = (currentIndex + 1) % questionBank.size
-
-            if (currentIndex != questionBank.size - 1)
+            if (currentIndex < questionBank.size - 1)
                 currentIndex = (currentIndex + 1) % questionBank.size
 
             updateQuestion()
         }
 
-        prevButton.setOnClickListener{
+        prevImageButton.setOnClickListener{
 //            currentIndex = if (currentIndex == 0) questionBank.size - 1
 //            else (currentIndex - 1) % questionBank.size
-
-           if (currentIndex != 0)
+           if (currentIndex > 0)
                currentIndex = (currentIndex - 1) % questionBank.size
 
             updateQuestion()
