f55c1e34c80e12ebffb42b358bff0952dc7610a7
thejiexa <thejiexxa@gmail.com>
2023-03-08 13:49:59 +1100

    lab 3

--- app/src/main/java/com/example/geoquiz/MainActivity.kt
+++ app/src/main/java/com/example/geoquiz/MainActivity.kt
@@ -7,8 +7,22 @@ import android.widget.Button
 import android.widget.ImageButton
 import android.widget.TextView
 import android.widget.Toast
+import kotlin.math.roundToInt
 
 private const val TAG = "MainActivity"
+
+private val questionBank = listOf(
+    Question(R.string.question_australia, true),
+    Question(R.string.question_oceans, true),
+    Question(R.string.question_mideast, false),
+    Question(R.string.question_africa, false),
+    Question(R.string.question_americas, true),
+    Question(R.string.question_asia, true)
+)
+private var currentIndex = 0
+private var answers = IntArray(questionBank.size)
+private var checked = IntArray(questionBank.size)
+
 class MainActivity : AppCompatActivity() {
 
     private lateinit var trueButton:Button
@@ -18,17 +32,6 @@ class MainActivity : AppCompatActivity() {
     private lateinit var questionTextView: TextView
     private lateinit var nextImageButton:ImageButton
     private lateinit var prevImageButton:ImageButton
-
-    private var currentIndex = 0
-
-    private val questionBank = listOf(
-        Question(R.string.question_australia, true),
-        Question(R.string.question_oceans, true),
-        Question(R.string.question_mideast, false),
-        Question(R.string.question_africa, false),
-        Question(R.string.question_americas, true),
-        Question(R.string.question_asia, true)
-    )
 
     override fun onStart() {
         super.onStart()
@@ -50,6 +53,31 @@ class MainActivity : AppCompatActivity() {
         super.onDestroy()
         Log.d(TAG, "onDestroy() called")
     }
+
+    private fun updateState(){
+        // LAB 3
+        if (checked[currentIndex] == 1){
+            falseButton.isEnabled = false
+            trueButton.isEnabled = false
+        }
+        else {
+            falseButton.isEnabled = true
+            trueButton.isEnabled = true
+        }
+
+        if (checked.sum() == answers.size){
+            val res = "${answers.sum()} of ${answers.size}: ${(answers.sum().toDouble() / answers.size * 100).roundToInt()} %"
+            Toast.makeText(this, res, Toast.LENGTH_LONG).show()
+
+            currentIndex = 0
+            checked = IntArray(questionBank.size)
+            answers = IntArray(questionBank.size)
+
+            falseButton.isEnabled = true
+            trueButton.isEnabled = true
+        }
+    }
+
     private fun updateQuestion() {
         val questionTextResId = questionBank[currentIndex].textResId
         questionTextView.setText(questionTextResId)
@@ -59,6 +87,11 @@ class MainActivity : AppCompatActivity() {
         val correctAnswer = questionBank[currentIndex].answer
         val messageResId = if (correctAnswer == userAnswer) R.string.correct_toast
         else R.string.incorrect_toast
+
+        // LAB 3
+        if (correctAnswer == userAnswer)
+            answers[currentIndex]++
+        checked[currentIndex]++
 
         Toast.makeText(this, messageResId, Toast.LENGTH_SHORT).show()
     }
@@ -76,37 +109,49 @@ class MainActivity : AppCompatActivity() {
 
         trueButton.setOnClickListener {
             checkAnswer(true)
+            updateState()
+            updateQuestion()
         }
 
         falseButton.setOnClickListener {
             checkAnswer(false)
+            updateState()
+            updateQuestion()
         }
 
         questionTextView.setOnClickListener {
             if (currentIndex < questionBank.size - 1)
                 currentIndex = (currentIndex + 1) % questionBank.size
 
+            updateState()
             updateQuestion()
         }
 
         nextImageButton.setOnClickListener{
-//            currentIndex = (currentIndex + 1) % questionBank.size
             if (currentIndex < questionBank.size - 1)
                 currentIndex = (currentIndex + 1) % questionBank.size
 
+            updateState()
             updateQuestion()
         }
 
         prevImageButton.setOnClickListener{
-//            currentIndex = if (currentIndex == 0) questionBank.size - 1
-//            else (currentIndex - 1) % questionBank.size
-           if (currentIndex > 0)
+            if (currentIndex > 0)
                currentIndex = (currentIndex - 1) % questionBank.size
 
+            updateState()
             updateQuestion()
         }
 
+        updateState()
         updateQuestion()
+
+        Log.d(TAG, "Current question index: $currentIndex")
+        try {
+            val question = questionBank[currentIndex]
+        } catch (ex: ArrayIndexOutOfBoundsException) {
+            Log.e(TAG, "Index was out of bounds", ex)
+        }
 
     }
 }
